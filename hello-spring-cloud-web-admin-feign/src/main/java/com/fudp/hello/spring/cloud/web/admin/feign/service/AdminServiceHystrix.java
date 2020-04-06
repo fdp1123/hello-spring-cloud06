964b1bbb46c7488be299c2b4b41f3dfca58739a3
Fudp <18311220159@163.com>
2020-04-06 10:33:56 +0800

    hello-spring-cloud

---
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/fudp/hello/spring/cloud/web/admin/feign/service/AdminServiceHystrix.java
@@ -0,0 +1,12 @@
+package com.fudp.hello.spring.cloud.web.admin.feign.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminServiceHystrix implements AdminService{
+
+    @Override
+    public String sayHi(String message) {
+        return "Hi，your message is :\"" + message + "\" but request error.";
+    }
+}
